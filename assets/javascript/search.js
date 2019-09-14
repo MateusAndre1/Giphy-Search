14e514fdb3603d8a3c68a741d49d2d03986abed8
Your Name <you@example.com>
2019-09-14 14:21:38 -0400

    html file complete with class and id to be targeted, pseudo code complete and added function to render array buttons

---
+++ assets/javascript/search.js
@@ -0,0 +1,8 @@
+// Pseudo Code
+// create an array of searchs for my giphys
+// call giphy api
+// link the api to my array of searchs
+// create a search bar for users to search giphys
+// allow users to click on their favorites
+// allow users to be displayed only their favorite api
+
