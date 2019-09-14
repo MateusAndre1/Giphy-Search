16ab27e0b64435921f89b539e843f501ef88ea71
Your Name <you@example.com>
2019-09-14 19:17:17 -0400

    added function for search and background image set

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -6,3 +6,35 @@
 // allow users to click on their favorites
 // allow users to be displayed only their favorite api
 
+// add an array of gifs to search, i chose some of my favorite movies and shows
+
+const buttons = ["The Office", "The Simpsons", "Harold & Kumar", "School of Rock", "Ron Swanson"];
+
+// create an array of buttons for my favorites, along with a button to remove selected search
+
+function displayButtons() {
+    $(".recent-search").empty();
+    for (let i = 0; i < buttons.length; i++) {
+        const buttonName = buttons[i];
+        
+        const button = `
+        <div class="wrap-buttons">
+        <button class="btn btn-delete fas fa-snowplow" data-name="${buttonName}"></button>
+        <button class="btn btn-search" data-name="${buttonName}">${buttonName}</button>
+        </div>
+        `;
+        $(".recent-search").append(button)
+    };
+};
+
+displayButtons();
+
+// display search results from user with a new button
+
+$("#submit-button").on("click", function(event) {
+    event.preventDefault();
+    let value = $("#search").val().trim();
+    buttons.push(value);
+    displayButtons();
+    console.log("Value: ", value)
+});
